19b57495f507743ca8b437c5b0d54ee25a585e4b
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-05-21 18:50:11 -0500

    alimentos controller

---
+++ app/Http/Controllers/alimentosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\alimentos;
+use Illuminate\Database\QueryException;
+use Exception;
+
+
+class alimentosController extends Controller
+{
+    //CRUD - Create, Read, Update, Delete - criar, ler, atualizar, deletar
+    
+    public function ListarAlimentos(){
+        try {
+            $alimentos = alimentos::all();
+            return response()->json($alimentos);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao listar alimentos: ',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function criarAlimento(){
+         try {
+            
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao criar alimento: ',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
+    }
+
+    public function atualizarAlimento(){
+         try {
+            
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao atualizar alimento: ',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
+    }
+    
+    public function deletarAlimento(){
+         try {
+            
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao deletar alimento: ',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+       
+    }
+
+    
+    
+}
