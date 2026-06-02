5ef02a919322543e4d8f476bf9dc6bf630a2e08b
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-06-02 17:10:53 -0500

    Rotas feito

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\alimentosController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('criarAlimento', [alimentosController::class, 'criarAlimento'])->name('criarAlimento');
+Route::get('listarAlimentos', [alimentosController::class, 'listarAlimentos'])->name('listarAlimentos');
+Route::post('atualizarAlimentos', [alimentosController::class, 'atualizarAlimentos'])->name('atualizarAlimentos');
+Route::delete('deletarAlimentos', [alimentosController::class, 'deletarAlimentos'])->name('deletarAlimentos');
